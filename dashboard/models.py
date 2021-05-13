abbdecb084ce8ca66d808d6d1f271f37a092857d
Jimi <69767677+JaaMata@users.noreply.github.com>
2021-05-13 22:01:06 +0100

    Added Tags

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,18 +5,26 @@ from django.db import models
 # Create your models here.
 
 
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.tag_name
+
+
 class Product(models.Model):
     name = models.CharField(null=True, max_length=200)
     price = models.FloatField(null=True)
     description = models.CharField(null=True, max_length=200)
     img = models.ImageField(upload_to="product-img", null=True)
+    tag = models.ManyToManyField(Tag,null=True)
 
     def __str__(self):
         return self.name
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(User,null=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(null=True, max_length=200)
 
     def __str__(self):
